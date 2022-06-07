02001f08ecb7f261af7eebc66d3891003ea275d3
1cristianb <crboschi@alumno.unlam.edu.ar>
2022-06-07 18:21:13 -0300

    Se agregó vuelos model

---
+++ model/VuelosModel.php
@@ -0,0 +1,26 @@
+<?php
+
+class VuelosModel
+{
+    private $database;
+
+    public function __construct($database)
+    {
+        $this->database = $database;
+    }
+
+    public function  traerTodosLosVuelos(){
+        return $this->database->query("SELECT * from vuelosSuborbitales");
+    }
+    public function buscarVuelo($valor) {
+
+        return $this->database->query("SELECT * from vuelosSuborbitales 
+      WHERE 
+      dia LIKE '%$valor%' 
+      OR vuelos LIKE '%$valor%' 
+      OR equipos LIKE '%$valor%'
+      OR duracion LIKE '%$valor%'
+      OR partida LIKE '%$valor%'");
+
+    }
+}
