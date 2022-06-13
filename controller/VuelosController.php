af1e99fc2bc437d024c20df4a2b5c2a8267f5abc
1cristianb <crboschi@alumno.unlam.edu.ar>
2022-06-13 13:52:24 -0300

    se modifico controller/VuelosController.php

--- controller/VuelosController.php
+++ controller/VuelosController.php
@@ -10,15 +10,29 @@ class VuelosController
         $this->printer=$printer;
     }
 
-   
     public function execute() {
         $data=[];
+        $vuelos=$this->vuelosModel->traerTodosLosVuelos();
         if(isset($_SESSION["email"])){
-            $data = ["sesionOn" =>"inline","sesionOn1"=>"none"];
+            $data = ["sesionOn" =>"inline","sesionOn1"=>"none","vuelosDevueltos"=>$vuelos];
         }else{
-            $data = ["sesion" =>"none","sesionOn1"=>"inline"];
+            $data = ["sesion" =>"none","sesionOn1"=>"inline","vuelosDevueltos"=>$vuelos];
         }
-      $this->printer->generateView('vuelosView.html',$data);
+        $this->printer->generateView('vuelosView.html',$data);
     }
 
+    public function buscarVuelosDisponibles() {
+        $destino=$_POST['destino'];
+        $origen=$_POST['origen'];
+        $dia=$_POST['dia'];
+
+        $vuelos=$this->vuelosModel->buscarVuelo($origen,$destino,$dia);
+
+        if(isset($_SESSION["email"])){
+            $suborbitales = ["sesionOn" =>"inline","sesionOn1"=>"none","vuelos"=>$vuelos];
+        }else{
+            $suborbitales = ["sesion" =>"none","sesionOn1"=>"inline","vuelos"=>$vuelos];
+        }
+        $this->printer->generateView('vuelosView.html',$suborbitales);
+    }
 }
