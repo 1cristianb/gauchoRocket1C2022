21c896da057733060ae25d4e161b225bf0b8c075
1cristianb <crboschi@alumno.unlam.edu.ar>
2022-06-13 13:51:35 -0300

    modificacion de model/VuelosModel.php

--- model/VuelosModel.php
+++ model/VuelosModel.php
@@ -10,17 +10,43 @@ class VuelosModel
     }
 
     public function  traerTodosLosVuelos(){
-        return $this->database->query("SELECT * from vuelosSuborbitales");
+        return $this->database->query("SELECT grillavuelos.dia,grillavuelos.horario,
+        grillavuelos.origen,grillavuelos.destino,nave.modelo,tipodeviaje.descripcion
+        FROM grillavuelos 
+        inner join nave on grillavuelos.equipoId=nave.id
+        inner join tipodeviaje on grillavuelos.tipoDeViajeId=tipodeviaje.id");
     }
-    public function buscarVuelo($valor) {
 
-        return $this->database->query("SELECT * from vuelosSuborbitales 
+    public function buscarVuelo($origen,$destino,$dia) {
+
+        return $this->database->query("SELECT grillavuelos.dia,grillavuelos.horario,
+        grillavuelos.origen,grillavuelos.destino,nave.modelo,tipodeviaje.descripcion
+        FROM grillavuelos 
+        inner join nave on grillavuelos.equipoId=nave.id
+        inner join tipodeviaje on grillavuelos.tipoDeViajeId=tipodeviaje.id
       WHERE 
-      dia LIKE '%$valor%' 
-      OR vuelos LIKE '%$valor%' 
-      OR equipos LIKE '%$valor%'
-      OR duracion LIKE '%$valor%'
-      OR partida LIKE '%$valor%'");
+      grillavuelos.origen LIKE '%$origen%' 
+    AND grillavuelos.destino LIKE '%$destino%' 
+      AND grillavuelos.dia LIKE '%$dia%'");
 
     }
+    public function buscarEscalas($origen,$destino,$dia){
+        $destinoDevuelto= $this->database->query("SELECT grillavuelos.dia,grillavuelos.horario,
+        grillavuelos.origen,grillavuelos.destino,nave.modelo,tipodeviaje.descripcion
+        FROM grillavuelos 
+        inner join nave on grillavuelos.equipoId=nave.id
+        inner join tipodeviaje on grillavuelos.tipoDeViajeId=tipodeviaje.id
+      WHERE 
+      grillavuelos.destino LIKE '%$destino%'");
+
+
+        $origenDevuelto= $this->database->query("SELECT grillavuelos.dia,grillavuelos.horario,
+        grillavuelos.origen,grillavuelos.destino,nave.modelo,tipodeviaje.descripcion
+        FROM grillavuelos 
+        inner join nave on grillavuelos.equipoId=nave.id
+        inner join tipodeviaje on grillavuelos.tipoDeViajeId=tipodeviaje.id
+      WHERE 
+      grillavuelos.destino LIKE '%$origen%'");
+    }
+
 }
